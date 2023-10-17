0d39670a8a77193953eb00b868530372528f1d11
Jose Cepero <ceperojose54@gmail.com>
2023-10-17 09:28:54 -0400

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -2,8 +2,12 @@ const db = require('../../data/db-config')
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
-function find() {
-  return db('users')
+async function find() {
+  const users = await db('users').select(
+    'user_id',
+    'username'
+  )
+  return users
 }
 
 /**
@@ -17,14 +21,20 @@ function findBy(filter) {
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-  return db('users').where('user_id',user_id).first()
+  return db('users')
+    .select(
+      'user_id',
+     'username'
+    )
+    .where('user_id',user_id)
+    .first()
 }
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
 async function add(user) {
-  const [newId]= await db('users').insert(users)
+  const [newId]= await db('users').insert(user)
   return findById(newId)
 }
 
